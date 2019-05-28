e7b56502344562271f60e2c891af566a6dd401e8
y_lin35@fanshaweonline.ca <46694893+TomLin19@users.noreply.github.com>
2019-05-28 17:46:23 -0400

    fianl

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 (() =>{
 	//stub
-	console.log('fired!');
+	console.log("fired!");
      // variables at the top
 	let headline = document.querySelector("h1"),
 	    subhead = document.querySelector("h3");
+	    theButton = document.querySelector("button")
 
      // function in the middle (what do you want to have happen)
-	function changeText(){
+	function changeText() {
 		headline.textContent = "Now I'm different!";
-		subhead.textContent = "hey me too,but that's ok.";
-
+		subhead.textContent = "hey me too,but that's ok."
 	}
-
+     // function calls / event handling at the bottom
 	//changeText();
-	theButton.addEventListener("Click", changeText);
+	theButton.addEventListener("click", changeText);
 })();
